c6d176e35c0bbf2faea699fe9d4f810aeea6d0db
akhripunkov <Alexander-e-mail@yandex.ru>
2024-12-12 11:44:30 +0300

    [HW] Added and tested generic method GetMax.

---
+++ DelegatesOtus/Extensions/CollectionExtension.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+
+namespace DelegatesOtus.Extensions;
+
+public static class CollectionExtension
+{
+    public static T GetMax<T>(this IEnumerable<T> collection, Func<T, float> convertToNumber) where T : class
+    {
+        if (collection == null)
+        {
+            throw new ArgumentException("The collection cannot be null or empty.");
+        }
+        
+        T maxItem = collection.Aggregate<T>((maxItem, element) =>
+        {
+            var converted = convertToNumber(element);
+            var convertedMax = convertToNumber(maxItem);
+            
+            return converted > convertedMax ? element : maxItem;
+        });
+        
+        return maxItem;
+    }
+}
---
+++ DelegatesOtus/Program.cs
@@ -0,0 +1,18 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+using DelegatesOtus.Extensions;
+
+Console.WriteLine($"GetMax function testing.");
+var items = new List<string> { "apple","smart-contract", "banana", "cherry", "date", "eggplant" };
+var maxItem = items.GetMax(item => item.Length);
+
+Console.WriteLine("List of words: ");
+var combined = string.Join(", ", items);
+Console.WriteLine(combined);
+Console.WriteLine($"Max item by length: {maxItem}");
+Console.WriteLine($"________________________");
+maxItem = items.GetMax(item => item.Last());
+Console.WriteLine("List of words: ");
+combined = string.Join(", ", items);
+Console.WriteLine(combined);
+Console.WriteLine($"Max item by last symbol: {maxItem}");
